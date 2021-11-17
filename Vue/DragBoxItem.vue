5d362720656468f924d163cc12617dcbf4d2dab6
jiahao_yu <495913582@qq.com>
2021-11-17 11:08:52 +0800

    add

---
+++ Vue/DragBoxItem.vue
@@ -0,0 +1,38 @@
+<!--  -->
+<template>
+    <div ref="container" class="d-flex" style="min-width: 200px; position: relative;">
+        <div :style="`width: 100%; height: 100%;padding-right:${resizeShow ? resizeWidth : '0'}`">
+            <slot>默认信息</slot>
+        </div>
+        <!-- 拖拽条 -->
+        <div v-if="resizeShow" class="dragbox-resize" :style="`width:${resizeWidth}`" />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        // 控制拖拽条的是否显示，默认显示
+        resizeShow: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    data() {
+        return {
+            resizeWidth: "15px",
+        };
+    },
+    methods: {},
+};
+</script>
+<style scoped>
+.dragbox-resize {
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    cursor: col-resize;
+    background-color: #d6d6d6;
+}
+</style>
---
+++ Vue/use.md
@@ -0,0 +1,20 @@
+```html
+<drag-box>
+    <drag-box-item>
+        <div> DIV1 </div> 
+    </drag-box-item>
+    <drag-box-item>
+        <div> DIV2 </div> 
+    </drag-box-item>
+    <drag-box-item :resizeShow="false">
+        <div> DIV3 </div> 
+    </drag-box-item>
+</drag-box>
+```
+
+## drag-box-item Attributes
+<br/>
+
+| 参数 | 说明 | 类型 | 可选值 | 默认值 |
+| ---- | ---- | ---- | ---- | ----- |
+| resizeShow | 是否显示拖动条 | boolean | - | true |
